445c1d42a966d52a122bf88fc5950c2709f858b2
Marc den Uil <marcuil75@gmail.com>
2017-06-13 14:34:24 +0200

    Initial commit #2

---
+++ app/src/main/java/com/example/marcu/movieapplication/presentation/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.marcu.movieapplication.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.marcu.movieapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
