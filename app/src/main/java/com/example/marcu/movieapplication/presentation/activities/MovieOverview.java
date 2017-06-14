7f328c7047d87bbbe3819a64b8e987729f022eb8
Marc den Uil <mcuil@avans.nl>
2017-06-14 18:55:02 +0200

    Login feature #2

--- app/src/main/java/com/example/marcu/movieapplication/presentation/activities/MovieOverview.java
+++ app/src/main/java/com/example/marcu/movieapplication/presentation/activities/MovieOverview.java
@@ -1,15 +1,33 @@
 package com.example.marcu.movieapplication.presentation.activities;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.marcu.movieapplication.R;
 
+import static com.example.marcu.movieapplication.presentation.activities.LoginActivity.JWT_STR;
+import static com.example.marcu.movieapplication.presentation.activities.LoginActivity.USER;
+
 public class MovieOverview extends AppCompatActivity {
+    private TextView textViewUserId;
+    private String jwt;
+    private int user;
+    private SharedPreferences prefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_overview);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        jwt = prefs.getString(JWT_STR, "");
+        user = prefs.getInt(USER, 0);
+
+        textViewUserId = (TextView)findViewById(R.id.textViewUserId);
+        textViewUserId.setText("User: " + user);
     }
 }
