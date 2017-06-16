8c4911beedcc3720dab1da79a6f9cd4b1965e599
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2017-06-16 11:51:32 +0200

    posten van rentals

--- app/src/main/java/com/example/marcu/movieapplication/presentation/activities/MovieOverview.java
+++ app/src/main/java/com/example/marcu/movieapplication/presentation/activities/MovieOverview.java
@@ -5,11 +5,16 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.marcu.movieapplication.R;
 import com.example.marcu.movieapplication.dataaccess.FilmsGetTask;
+import com.example.marcu.movieapplication.dataaccess.RegisterPostTask;
+import com.example.marcu.movieapplication.dataaccess.RentalPostTask;
 import com.example.marcu.movieapplication.domain.Film;
 import com.example.marcu.movieapplication.presentation.adapters.FilmsAdapter;
 
@@ -18,7 +23,7 @@ import java.util.ArrayList;
 import static com.example.marcu.movieapplication.presentation.activities.LoginActivity.JWT_STR;
 import static com.example.marcu.movieapplication.presentation.activities.LoginActivity.USER;
 
-public class MovieOverview extends AppCompatActivity implements FilmsGetTask.OnFilmAvailable {
+public class MovieOverview extends AppCompatActivity implements FilmsGetTask.OnFilmAvailable, RentalPostTask.PutSuccessListener, AdapterView.OnItemClickListener {
 
     private TextView textViewUserId;
     private String jwt;
@@ -43,6 +48,7 @@ public class MovieOverview extends AppCompatActivity implements FilmsGetTask.OnF
 
         getFilms();
         listViewFilms = (ListView) findViewById(R.id.filmsListview);
+        listViewFilms.setOnItemClickListener(this);
     }
 
     public void getFilms(){
@@ -56,6 +62,26 @@ public class MovieOverview extends AppCompatActivity implements FilmsGetTask.OnF
         films.add(film);
         filmsAdapter = new FilmsAdapter(this,getLayoutInflater(),films);
         listViewFilms.setAdapter(filmsAdapter);
+    }
+
+    private void loan(String url){
+        String[] urls = new String[]{url};
+        RentalPostTask task = new RentalPostTask(this);
+        task.execute(urls);
+    }
+
+    @Override
+    public void putSuccessful(Boolean successful) {
+        if(successful){
+            Toast.makeText(this, "Film has been added too your rentals!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Adding film failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+        Film f = films.get(position);
     }
 
 }
