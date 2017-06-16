51af6f6618cf3c10d1e38c07805df4e7fadcca9c
Marc den Uil <marcuil75@gmail.com>
2017-06-16 12:49:54 +0200

    changed menu & changed logout

--- app/src/main/java/com/example/marcu/movieapplication/presentation/drawer/Drawer.java
+++ app/src/main/java/com/example/marcu/movieapplication/presentation/drawer/Drawer.java
@@ -2,12 +2,17 @@ package com.example.marcu.movieapplication.presentation.drawer;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.example.marcu.movieapplication.R;
 import com.example.marcu.movieapplication.presentation.activities.LoginActivity;
 import com.example.marcu.movieapplication.presentation.activities.MovieOverview;
 import com.example.marcu.movieapplication.presentation.activities.MyRentalsActivity;
+
+import static com.example.marcu.movieapplication.presentation.activities.LoginActivity.JWT_STR;
+import static com.example.marcu.movieapplication.presentation.activities.LoginActivity.USER;
 
 /**
  * Created by marcu on 6/16/2017.
@@ -19,14 +24,16 @@ public class Drawer {
     private Context context;
     private Intent intent;
 
-    private String jwt;
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
 
     public Drawer(Context context, int id, String jwt, int user) {
 
         Log.i(tag, "user: " + user);
 
         this.context = context;
-        this.jwt = jwt;
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        editor = prefs.edit();
 
         switch (id) {
             case R.id.nav_movie_overview:
@@ -35,7 +42,6 @@ public class Drawer {
                 context.startActivity(intent);
                 break;
             case R.id.nav_rentals:
-                Log.i("drawer", "rentals");
                 intent = new Intent(context, MyRentalsActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(intent);
@@ -45,6 +51,9 @@ public class Drawer {
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
                         Intent.FLAG_ACTIVITY_CLEAR_TASK |
                         Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                editor.remove(JWT_STR);
+                editor.apply();
                 context.startActivity(intent);
                 break;
             default :
